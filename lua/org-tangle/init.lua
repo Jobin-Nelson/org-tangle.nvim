3cf8a567f58cc7613174893b1d3925f9cf959819
Jobin Nelson <jobinnelson369@gmail.com>
2023-05-21 19:14:45 +0530

    match filetype

--- lua/org-tangle/init.lua
+++ lua/org-tangle/init.lua
@@ -12,15 +12,21 @@ local directive_query = vim.treesitter.query.parse(
   ]]
 )
 
-local function in_valid_headers(text)
+local block_query = vim.treesitter.query.parse(
+  lang,
+  [[
+(block
+  parameter: (expr) @x (#match? @x "lisp"))
+  ]]
+)
+
+local function check_valid_headers(text)
   for idx, header in ipairs(valid_headers) do
     if text == header then
       table.remove(valid_headers, idx)
-      return true
+      return
     end
   end
-
-  return false
 end
 
 local function get_root(bufnr)
@@ -31,35 +37,40 @@ end
 
 local function get_target_file()
   -- local bufnr = vim.api.nvim_get_current_buf()
-  local bufnr = 3
+  local bufnr = 4
   if vim.bo[bufnr].filetype ~= 'org' then
     error('Tangle can only be performed on an org file')
   end
 
   local root = get_root(bufnr)
-  local valid_level = 3
 
   local target_file = ''
   for id, node in directive_query:iter_captures(root, bufnr, 0, -1) do
     local text = vim.treesitter.get_node_text(node, bufnr)
-    if vim.tbl_isempty(valid_headers) and valid_level == 0 then
+    if vim.tbl_isempty(valid_headers) then
       target_file = text
       break
     end
-    if in_valid_headers(text) then
-      valid_level = valid_level - 1
-    end
+    check_valid_headers(text)
   end
 
   if target_file == '' then
     error('No target file found from Org headers')
   end
+  vim.print(target_file)
   return target_file
 end
 
 local function create_file(target)
   local target_filetype = vim.filetype.match({ filename = target })
-  vim.print(target_filetype)
+  local bufnr = 4
+  local root = get_root(bufnr)
+
+  local changes = {}
+  for id, node in block_query:iter_captures(root, bufnr, 0, -1) do
+    local text = vim.treesitter.get_node_text(node:next_named_sibling(), bufnr)
+    table.insert(changes, text)
+  end
 end
 
 create_file(get_target_file())
@@ -71,3 +82,4 @@ create_file(get_target_file())
 -- end
 --
 -- return M
+-- lua vim.keymap.set('n', '<leader>r', ':luafile ~/playground/projects/org-tangle.nvim/lua/org-tangle/init.lua<cr>')
